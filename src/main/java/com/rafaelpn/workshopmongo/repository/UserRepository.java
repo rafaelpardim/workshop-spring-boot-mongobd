14b15a10ae7dba0abaab0caebdf91d7648a7cb79
Rafael Pardim <ppardim27@gmail.com>
2024-10-03 16:24:27 -0300

    usuário com DELETE

---
+++ src/main/java/com/rafaelpn/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.rafaelpn.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaelpn.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	
+
+}
--- src/main/java/com/rafaelpn/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rafaelpn/workshopmongo/resources/UserResource.java
@@ -1,28 +1,54 @@
 package com.rafaelpn.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafaelpn.workshopmongo.domain.User;
+import com.rafaelpn.workshopmongo.dto.UserDTO;
+import com.rafaelpn.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>>findAll(){
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "Alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
-		return ResponseEntity.ok().body(list);
-		
+	public ResponseEntity<List<UserDTO>>findAll() {
+		List<User> list = service.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);		
 	}
 
+	@RequestMapping(value= "/{id}",method=RequestMethod.GET)
+	public ResponseEntity<UserDTO>findById(@PathVariable String id) {
+		User obj = service.findById(id);		
+		return ResponseEntity.ok().body(new UserDTO(obj));		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void>insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+				
+	}
+	
+	@RequestMapping(value= "/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Void>delete(@PathVariable String id) {
+		service.delete(id);	
+		return ResponseEntity.noContent().build();
+	}
 }
